7a0fe139e70d56b327eabfb5388bf34e9878088b
Tom Trval <trval@kajot.cz>
2026-07-03 00:07:02 +0200

    chore: load transparent source extension

---
+++ extensions/index.ts
@@ -0,0 +1,3 @@
+// Source entrypoint for Pi package loading.
+// The npm library entrypoint remains dist/index.js, but Pi loads this readable source path.
+export { default } from "../src/index.ts";
